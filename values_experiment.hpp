56786318db8ccc0043233c676b07a470a1e96291
Adis7i <Adhyastha.abiyyu.azlee@gmail.com>
2026-01-30 21:58:42 +0700

    use std::visit

--- values_experiment.hpp
+++ values_experiment.hpp
@@ -1,13 +1,10 @@
 #pragma once
 
-#include <cstdint>
-#include <array>
-#include <iostream>
 #include <variant>
 #include <type_traits>
-#include <span>
 #include <functional>
 #include <bit>
+#include <cstdint>
 
 #include "commons.hpp"
 #include "exceptions.hpp"
@@ -71,17 +68,14 @@ constexpr bool is_ref_ctgry(const type_code::Tcode& code) noexcept {
 }
 
 template <typename T, typename... Ts>
-struct is_within_variant {
+struct assignable_within_variant {
 	static constexpr bool value = false;
 };
 
 template <typename T, typename... Ts>
-struct is_within_variant<T, std::variant<Ts...>> {
-	static constexpr bool value = std::disjunction<std::is_same<T, Ts>...>::value;
+struct assignable_within_variant<T, std::variant<Ts...>> {
+	static constexpr bool value = std::disjunction<std::is_assignable<Ts&, T&>...>::value;
 };
-
-
-
 
 struct TrackingSpan {
 	ArrT viewer;
@@ -104,43 +98,50 @@ struct TrackingSpan {
 	}
 
 	void track_reset() noexcept { curr_idx = 0; }
+
+	std::size_t consume_amount() const noexcept { return viewer.size(); }
 };
 
 template <typename T>
 struct TrackingReference : public std::reference_wrapper<T> {
 	bool filled = false;
 	using std::reference_wrapper<T>::get;
-	bool insert(const T& val) {
+	
+
+	bool push_back(const T& val) {
 		if(filled)
 			return false;
 		this->get() = val;
 		return (filled = true);
 	}
 
-	bool insert(T&& val) {
-		if(filled)	 
+	bool push_back(T&& val) {
+		if(filled)
 			return false;
 		this->get() = std::move(val);
 		return (filled = true);
 	}
 
+	template <typename ParamType>
+	bool push_back(const ParamType& _) {
+		std::string signature = __PRETTY_FUNCTION__;
+		signature = signature.substr(signature.find('['));
+		throw except::ParseError("Wrong type : " + signature);
+	}
+
 	void track_reset() noexcept { filled = false; }
+	std::size_t consume_amount() const noexcept { return 1; }
 };
 
 using IntRef = TrackingReference<IntT>;
 using DobRef = TrackingReference<DobT>;
 using StrRef = TrackingReference<StrT>;
 
-template <typename T>
-struct to_ref {
-	using type = TrackingReference<T>;	
-};
-
 template <typename GetType, typename VariantType>
 GetType& ce_get(VariantType& ins, std::string_view error_msg) {  // Custom Error
 	if(std::holds_alternative<GetType>(ins))
 		return std::get<GetType>(ins);
-	else 
+	else
 		throw std::invalid_argument(("(Discriminator : " + std::to_string(ins.index()) + ") ").append(error_msg));
 }
 
@@ -160,125 +161,43 @@ class BoundValue {
 	val_type value;
 
 	void reset() {
-		// we use value index, because it's not messier
-		switch(value.index()) {
-			case 0 : return;
-			case 1 :
-				std::get<std::variant_alternative_t<1, val_type>>(value).track_reset();
-				break;
-
-			case 2 :
-				std::get<std::variant_alternative_t<2, val_type>>(value).track_reset();
-				break;
-
-			case 3 :
-				std::get<std::variant_alternative_t<3, val_type>>(value).track_reset();
-				break;
-
-			case 4 :
-				std::get<std::variant_alternative_t<4, val_type>>(value).track_reset();
-				break;
-		}
-	}
-
-	static bool fill_str(void* var, type_code::Tcode code, BoundValue& ins) {
-		if(!var || (code != type_code::kStr))
-			throw except::ParseError("fill_str : invalid argument");
+		std::visit([](auto&& arg){
+			using T = std::decay_t<decltype(arg)>;
+			if constexpr (!std::is_same_v<T, std::monostate>) {
+				arg.track_reset();
+			}
+		}, this->value);
 		
-		return ce_get<StrRef>(ins.value, "fill_str : get failed")
-				.insert(*reinterpret_cast<StrT*>(var));
-	}
-
-	static bool fill_int(void* var, type_code::Tcode code, BoundValue& ins) {
-		if(!var || (code != type_code::kInt))
-			throw except::ParseError("fill_int : invalid argument");
-		
-		return ce_get<IntRef>(ins.value, "fill_int : get failed")
-				.insert(*reinterpret_cast<IntT*>(var));
-	}
-
-	static bool fill_dob(void* var, type_code::Tcode code, BoundValue& ins) {
-		if(!var || (code != type_code::kDob))
-			throw except::ParseError("fill_dob : invalid argument");
-		
-		return ce_get<DobRef>(ins.value, "fill_dob : get failed")
-				.insert(*reinterpret_cast<DobT*>(var));
-	}
-
-	static bool fill_arr(void* var, type_code::Tcode code, BoundValue& ins) {
-		if(!var) 
-			throw except::ParseError("fill_arr : \"var\" argument is a nullptr");
-
-		TrackingSpan& arr = 
-			ce_get<TrackingSpan>(ins.value, "fill_arr : get failed");
-
-		switch (code.value())
-		{
-		case type_code::kInt.value() : return arr.push_back(*reinterpret_cast<IntT*>(var));
-		case type_code::kDob.value() : return arr.push_back(*reinterpret_cast<DobT*>(var));
-		case type_code::kStr.value() : return arr.push_back(*reinterpret_cast<StrT*>(var));
-		default: throw except::ParseError(
-			(std::string("fill_arr : type_code of ") +
-			type_code::code_to_str(code)) +
-			" Is unacceptable"
-		);
-		}
-	}
-
-	static bool fill_fail(void*, type_code::Tcode, BoundValue&) { return false; }
-	// Boolean value indicate BoundValue is done fetching. not an error
-	bool (*fill_method)(void*, type_code::Tcode, BoundValue&) = fill_fail;
-
-	template <typename T>
-	void set_fill_method() {
-		if constexpr (std::is_same_v<T, IntRef>) 
-			fill_method = fill_int;
-		else if constexpr (std::is_same_v<T,DobRef>) 
-			fill_method = fill_dob;
-		else if constexpr (std::is_same_v<T, StrRef>)
-			fill_method = fill_str;
-		else if constexpr (std::is_same_v<T, TrackingSpan>)
-			fill_method = fill_arr;
-		else 
-			throw except::SetupError("Unsupported type set_fill_method failed");
-	}
-
-	template <typename T>
-	std::size_t consume_amnt_impl() const noexcept { return 0; }
-
-	template<typename T>
-	std::size_t consume_amnt_impl<typename to_ref<T>::type>() const noexcept {
-		return 1;
-	}
-
-	template <>
-	std::size_t consume_amnt_impl<TrackingSpan>() const noexcept {
-		return std::get<TrackingSpan>(this->value).viewer.size();
 	}
 
 	public :
 
 	auto opc() { // open parsing context
 		this->reset();
-		return [&](void* val, type_code::Tcode code) -> bool {
-			return fill_method(val, code, *this);
+		return [this](auto&& var) {
+			std::visit([&var](auto&& data) {
+				using T = std::decay_t<decltype(data)>;
+				if constexpr (std::is_same_v<T, std::monostate>) {
+					return false;
+				} else
+					data.push_back(var);
+				
+			}, this->value);
 		};
 	}
 
 	template <typename T>
-	typename std::enable_if_t<is_within_variant<typename to_ref<T>::type, val_type>::value, void>
-	bind(T& ref) {
-		this->value = std::ref(ref);
-		set_fill_method<to_ref<T>::type>();
-	}
+	typename std::enable_if_t<assignable_within_variant<T, val_type>::value, void>
+	bind(T& ref) { this->value = std::ref(ref); }
 
-	void bind(ArrT arr) {
-		this->value = TrackingSpan(arr);
-		set_fill_method<TrackingSpan>();
-	}
-
-	std::size_t consume_amnt() const {
-
+	std::size_t consume_amnt() const noexcept {
+		return std::visit([](auto&& arg) -> std::size_t {
+			using T = std::decay_t<decltype(arg)>;
+			if constexpr (std::is_same_v<T, std::monostate>)
+				return 0;
+			else 
+				return arg.consume_amount();
+		}, this->value);
 	}
 
 
@@ -302,4 +221,5 @@ const TypeCodeT& codeStr = values::type_code::kStr;
 const TypeCodeT& codeInt = values::type_code::kInt;
 const TypeCodeT& codeDob = values::type_code::kDob;
 const TypeCodeT& codeArr = values::type_code::kRangedArr;
+
 }
